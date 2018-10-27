e3048e6508ea261ca0ef8d0aad40cf9f9b0784e2
XinAnzzZ <1150989249@qq.com>
2018-10-27 15:30:08 +0800

    fix

--- 03-LinkedList/src/com/alibaba/xinan/Solution.java
+++ 03-LinkedList/src/com/alibaba/xinan/Solution.java
@@ -1,6 +1,8 @@
 package com.alibaba.xinan;
 
 /**
+ * LeetCode Question no.203 solution
+ *
  * @author XinAnzzZ
  * @date 2018/10/26 13:54
  */
@@ -16,18 +18,41 @@ public class Solution {
     }
 
     public ListNode removeElements(ListNode head, int val) {
-        if (head == null) {
-            return null;
-        }
 
-        while (head.val == val) {
-            // 头结点就等于要移除的元素
+        // 头结点就等于要移除的元素
+        while (head != null && head.val == val) {
             ListNode target = head;
             head = head.next;
             target.next = null;
         }
-        return null;
+
+        if (head == null) {
+            return null;
+        }
+
+        // 非头结点就等于要移除的元素
+        ListNode pre = head;
+        while (pre.next != null) {
+            if (pre.next.val == val) {
+                ListNode target = pre.next;
+                pre.next = target.next;
+                target.next = null;
+                continue;
+            }
+            pre = pre.next;
+        }
+        return head;
     }
 
+    /**
+     * 递归的方式解决这个问题
+     */
+    public ListNode removeElementsByRecursive(ListNode head, int val) {
+        if (head == null) {
+            return null;
+        }
 
+        head.next = removeElementsByRecursive(head.next, val);
+        return head.val == val ? head.next : head;
+    }
 }
